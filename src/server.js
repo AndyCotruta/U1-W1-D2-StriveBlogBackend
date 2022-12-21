2c114eb8d6b79f6974f359c099fb12cc108c0062
AndyCotruta <acotruta18@gmail.com>
2022-12-21 15:18:53 +0100

    Added blogs API

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
+import cors from "cors";
 import listEndpoints from "express-list-endpoints";
-import authorsRouter from "./blogs/index.js";
+import authorsRouter from "./authors/index.js";
+import blogsRouter from "./blogs/index.js";
 
 const server = express();
 const port = 3001;
 
+server.use(cors());
 server.use(express.json());
 
 server.use("/authors", authorsRouter);
+server.use("/blogs", blogsRouter);
 
 server.listen(port, () => {
   console.table(listEndpoints(server));

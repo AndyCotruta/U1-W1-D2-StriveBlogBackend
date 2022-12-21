e85eb71191f8ed2578bcdcaa3957a17e97732888
AndyCotruta <acotruta18@gmail.com>
2022-12-21 16:48:45 +0100

    errorHandlers and validator implemented

--- src/blogs/index.js
+++ src/blogs/index.js
@@ -3,7 +3,8 @@ import fs from "fs";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import uniqid from "uniqid";
-import { request } from "http";
+import httpErrors from "http-errors";
+import { checksBlogPostSchema, triggerBadRequest } from "./validator.js";
 
 const blogsPATHname = join(
   dirname(fileURLToPath(import.meta.url)),
@@ -12,6 +13,8 @@ const blogsPATHname = join(
 
 const blogsRouter = express.Router();
 
+const { NotFound, Unauthorized, BadRequest } = httpErrors;
+
 const getBlogs = () => JSON.parse(fs.readFileSync(blogsPATHname));
 const writeBlogs = (blogsArray) =>
   fs.writeFileSync(blogsPATHname, JSON.stringify(blogsArray));
@@ -19,54 +22,94 @@ const writeBlogs = (blogsArray) =>
 // ........................................CRUD operations..................................
 
 // 1. Create blog
-blogsRouter.post("/", (req, res) => {
-  const newBlog = {
-    ...req.body,
-    _id: uniqid(),
-    createdAt: new Date(),
-  };
-  const blogsArray = getBlogs();
-  blogsArray.push(newBlog);
-  writeBlogs(blogsArray);
-  res.status(200).send(`Blog with id ${newBlog._id} was created successfully`);
-});
+blogsRouter.post(
+  "/",
+  checksBlogPostSchema,
+  triggerBadRequest,
+  (req, res, next) => {
+    try {
+      const newBlog = {
+        ...req.body,
+        _id: uniqid(),
+        createdAt: new Date(),
+      };
+      const blogsArray = getBlogs();
+      blogsArray.push(newBlog);
+      writeBlogs(blogsArray);
+      res
+        .status(200)
+        .send(`Blog with id ${newBlog._id} was created successfully`);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 // 2. Read all blogs
-blogsRouter.get("/", (req, res) => {
-  const blogs = getBlogs();
-  res.send(blogs);
+blogsRouter.get("/", (req, res, next) => {
+  try {
+    const blogs = getBlogs();
+    res.send(blogs);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // 3. Read a blog by ID
-blogsRouter.get("/:blogId", (req, res) => {
-  const blogId = req.params.blogId;
-  const blogsArray = getBlogs();
-  const searchedBlog = blogsArray.find((blog) => blog._id === blogId);
-  res.send(searchedBlog);
+blogsRouter.get("/:blogId", (req, res, next) => {
+  try {
+    const blogId = req.params.blogId;
+    const blogsArray = getBlogs();
+    const searchedBlog = blogsArray.find((blog) => blog._id === blogId);
+    if (searchedBlog) {
+      res.send(searchedBlog);
+    } else {
+      next(NotFound(`Blog with id ${blogId} not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // 4. Update a blog
-blogsRouter.put("/:blogId", (req, res) => {
-  const blogId = req.params.blogId;
-  const blogsArray = getBlogs();
-  const oldBlogIndex = blogsArray.findIndex((blog) => blog._id === blogId);
-  const oldBlog = blogsArray[oldBlogIndex];
-  const updatedBlog = {
-    ...oldBlog,
-    ...req.body,
-    updatedAt: new Date(),
-  };
-  blogsArray[oldBlogIndex] = updatedBlog;
-  writeBlogs(blogsArray);
-  res.send(updatedBlog);
+blogsRouter.put("/:blogId", (req, res, next) => {
+  try {
+    const blogId = req.params.blogId;
+    const blogsArray = getBlogs();
+    const oldBlogIndex = blogsArray.findIndex((blog) => blog._id === blogId);
+    if (oldBlogIndex !== -1) {
+      const oldBlog = blogsArray[oldBlogIndex];
+      const updatedBlog = {
+        ...oldBlog,
+        ...req.body,
+        updatedAt: new Date(),
+      };
+      blogsArray[oldBlogIndex] = updatedBlog;
+      writeBlogs(blogsArray);
+      res.send(updatedBlog);
+    } else {
+      next(NotFound(`Blog with id ${blogId} not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // 5. Delete a blog
-blogsRouter.delete("/:blogId", (req, res) => {
-  const blogId = req.params.blogId;
-  const blogsArray = getBlogs();
-  const filteredBlogsArray = blogsArray.filter((blog) => blog._id !== blogId);
-  res.status(204).send();
+blogsRouter.delete("/:blogId", (req, res, next) => {
+  try {
+    const blogId = req.params.blogId;
+    const blogsArray = getBlogs();
+    const filteredBlogsArray = blogsArray.filter((blog) => blog._id !== blogId);
+    if (filteredBlogsArray.length !== blogsArray.length) {
+      writeBlogs(filteredBlogsArray);
+      res.status(204).send();
+    } else {
+      next(NotFound(`Blog with id ${blogId} not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default blogsRouter;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ import cors from "cors";
 import listEndpoints from "express-list-endpoints";
 import authorsRouter from "./authors/index.js";
 import blogsRouter from "./blogs/index.js";
+import {
+  genericErrorHandler,
+  notFoundHandler,
+  badRequestHandler,
+  unauthorizedHandler,
+} from "./errorHandlers.js";
 
 const server = express();
 const port = 3001;
@@ -10,8 +16,17 @@ const port = 3001;
 server.use(cors());
 server.use(express.json());
 
+// ..................ENDPOINTS..................
+
 server.use("/authors", authorsRouter);
 server.use("/blogs", blogsRouter);
+
+// ..................ERROR HANDLERS............
+
+server.use(badRequestHandler); // 400
+server.use(unauthorizedHandler); // 401
+server.use(notFoundHandler); // 404
+server.use(genericErrorHandler); // 500
 
 server.listen(port, () => {
   console.table(listEndpoints(server));
